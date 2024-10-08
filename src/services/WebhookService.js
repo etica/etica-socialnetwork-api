a8ad08997f736f52c46c9ac321243100cc1b8ad4
Kevin Wad <k.w.x.m.r@protonmail.com>
2024-10-08 19:29:20 +0200

    Implementation of Discord webhooks started

--- src/services/WebhookService.js
+++ src/services/WebhookService.js
@@ -88,7 +88,7 @@ class WebhookService {
 
     try {
       const payload = {
-        content: `_NEW PROPOSAL_ **NEW PROPOSAL**`,
+        content: `_NEW RESEARCH PROPOSAL_ `,
         embeds: [{
           title: proposal.title.substring(0, 256), // Discord title limit is 256 characters
           description: proposal.description.substring(0, 1000), // Discord description limit is 4096 characters
