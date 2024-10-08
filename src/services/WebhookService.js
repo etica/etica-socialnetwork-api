21592faf4fa6fcf90caf70a638f5428fee66e061
Kevin Wad <k.w.x.m.r@protonmail.com>
2024-10-08 19:37:48 +0200

    Implementation of Discord webhooks started

--- src/services/WebhookService.js
+++ src/services/WebhookService.js
@@ -88,7 +88,7 @@ class WebhookService {
 
     try {
       const payload = {
-      content: `_NEW RESEARCH PROPOSAL_ (votes: ${moment.unix(startTimeNumber).format("MMM Do YYYY hh:mm") - moment.unix(endTimeNumber).format("MMM Do YYYY hh:mm")})`,
+      content: `_NEW RESEARCH PROPOSAL_ (votes: ${moment.unix(startTimeNumber).format("MMM Do YYYY hh:mm")} - ${moment.unix(endTimeNumber).format("MMM Do YYYY hh:mm")})`,
         embeds: [{
           title: proposal.title.substring(0, 256), // Discord title limit is 256 characters
           description: proposal.description.substring(0, 1000), // Discord description limit is 4096 characters
